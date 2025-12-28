c62c6f27aad3f7da21c99673073167beebe9c702
patel0700 <patel0700@saskpolytech.ca>
2025-12-27 19:56:23 -0600

    WIP: latest changes

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -34,15 +34,15 @@ const Login = () => {
                      setEmail(e.target.value)
                   }}
                 
-                className="text-white outline-none  border-2 border-emrald-600 rounded-full text-white-xl py-3 px-5 bg-transparent placeholder:text-white" type="email" placeholder='Enter your email' />
+                className="text-white outline-none  border-2 border-emerald-600 rounded-full text-white-xl py-3 px-5 bg-transparent placeholder:text-white" type="email" placeholder='Enter your email' />
                 
                 <input 
                  value={password}
                  onChange={(e) => {
                   setPassword(e.target.value)
                  }}
-                  className="mt-4 text-whtie outline-none border-2 border-emrald-600 rounded-full text-white-xl py-3 px-5 bg-transparent placeholder:text-white"  type="password" placeholder='Enter your password' />
-                <button className="mt-5 text-white outline-none  border-2  bg-emerald-600 border-emrald-600 rounded-full text-white-xl py-3 px-5 placeholder:text-white" >Login</button>
+                  className="mt-4 text-white outline-none border-2 border-emerald-600 rounded-full text-white-xl py-3 px-5 bg-transparent placeholder:text-white"  type="password" placeholder='Enter your password' />
+                <button className="mt-5 text-white outline-none  border-2 border-emerald-600  bg-emerald-600 border-emrald-600 rounded-full text-white-xl py-3 px-5 placeholder:text-white" >Login</button>
 
                 
             </form>
